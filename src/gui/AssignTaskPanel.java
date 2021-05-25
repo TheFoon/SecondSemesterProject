be9c1ee39cadd84ac48fe6d4cc69726c9e07db96
tamaspalya <tamas.palya01@gmail.com>
2021-05-25 15:11:11 +0200

    Panel stubs added
    AssignTaskPanel and HousingUnitPanel for planned implementations were added

---
+++ src/gui/AssignTaskPanel.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JPanel;
+
+public class AssignTaskPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public AssignTaskPanel() {
+
+	}
+
+}
---
+++ src/gui/HousingUnitPanel.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JPanel;
+
+public class HousingUnitPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public HousingUnitPanel() {
+
+	}
+
+}
