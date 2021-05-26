9e33af233dd8675cf351049eb1abbbf131ca3992
tamaspalya <tamas.palya01@gmail.com>
2021-05-26 11:14:28 +0200

    Update ReportDefectPanel.java
    Sketch of report defect panel

--- src/gui/ReportDefectPanel.java
+++ src/gui/ReportDefectPanel.java
@@ -4,11 +4,11 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class ReportDefectPanel extends JPanel {
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTextField textField_2;
 
 	/**
 	 * Create the panel.
@@ -19,42 +19,41 @@ public class ReportDefectPanel extends JPanel {
 		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Report Defect Panel");
-		lblNewLabel.setBounds(180, 23, 132, 14);
+		lblNewLabel.setBounds(189, 24, 132, 14);
 		add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(90, 85, 46, 14);
+		lblNewLabel_1.setBounds(126, 59, 67, 14);
 		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(90, 120, 46, 14);
+		lblNewLabel_2.setBounds(126, 115, 67, 14);
 		add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setBounds(90, 163, 46, 14);
+		lblNewLabel_3.setBounds(126, 171, 67, 14);
 		add(lblNewLabel_3);
 		
 		textField = new JTextField();
-		textField.setBounds(159, 82, 86, 20);
+		textField.setBounds(126, 84, 132, 20);
 		add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(159, 117, 86, 20);
+		textField_1.setBounds(126, 140, 132, 20);
 		add(textField_1);
 		textField_1.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(159, 160, 170, 74);
-		add(textField_2);
-		textField_2.setColumns(10);
-		
 		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(405, 305, 89, 23);
+		btnNewButton.setBounds(402, 305, 89, 23);
 		add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(306, 305, 89, 23);
+		btnNewButton_1.setBounds(303, 305, 89, 23);
 		add(btnNewButton_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(126, 198, 236, 96);
+		add(textArea);
 	}
 }
