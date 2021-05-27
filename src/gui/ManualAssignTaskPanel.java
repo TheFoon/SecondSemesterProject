e9d83fc06700bc59c6b95c0e7601ccc716c12055
tamaspalya <tamas.palya01@gmail.com>
2021-05-27 12:34:49 +0200

    Update ManualAssignTaskPanel.java

--- src/gui/ManualAssignTaskPanel.java
+++ src/gui/ManualAssignTaskPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class ManualAssignTaskPanel extends JPanel {
 
@@ -18,11 +19,11 @@ public class ManualAssignTaskPanel extends JPanel {
 		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Cleaner");
-		lblNewLabel.setBounds(46, 51, 46, 14);
+		lblNewLabel.setBounds(46, 55, 46, 14);
 		add(lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(46, 76, 110, 22);
+		comboBox.setBounds(100, 51, 110, 22);
 		add(comboBox);
 		
 		JLabel lblNewLabel_1 = new JLabel("Shift");
@@ -30,7 +31,7 @@ public class ManualAssignTaskPanel extends JPanel {
 		add(lblNewLabel_1);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(46, 134, 110, 22);
+		comboBox_1.setBounds(100, 105, 110, 22);
 		add(comboBox_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Task");
@@ -38,8 +39,12 @@ public class ManualAssignTaskPanel extends JPanel {
 		add(lblNewLabel_2);
 		
 		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(46, 192, 110, 22);
+		comboBox_2.setBounds(100, 163, 110, 22);
 		add(comboBox_2);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(351, 266, 89, 23);
+		add(btnNewButton);
 
 	}
 	
