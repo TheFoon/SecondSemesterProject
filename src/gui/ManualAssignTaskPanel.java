75697ed31b2eb588f048ce0e0fb7c1a880c19d9a
tamaspalya <tamas.palya01@gmail.com>
2021-05-27 12:23:37 +0200

    Update ManualAssignTaskPanel.java

--- src/gui/ManualAssignTaskPanel.java
+++ src/gui/ManualAssignTaskPanel.java
@@ -1,6 +1,9 @@
 package gui;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class ManualAssignTaskPanel extends JPanel {
 
@@ -8,7 +11,31 @@ public class ManualAssignTaskPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public ManualAssignTaskPanel() {
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(46, 51, 46, 14);
+		add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(46, 76, 110, 22);
+		add(comboBox);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(46, 109, 46, 14);
+		add(lblNewLabel_1);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(46, 134, 110, 22);
+		add(comboBox_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(46, 167, 46, 14);
+		add(lblNewLabel_2);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setBounds(46, 192, 110, 22);
+		add(comboBox_2);
 
 	}
-
 }
